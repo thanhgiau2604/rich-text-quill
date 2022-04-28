947bb56862e8c3e102b6c5e824a7c8dcd05a5540
giau.nguyen <giau.nguyen@executionlab.asia>
2022-04-28 13:25:19 +0700

    update checking webview

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,34 @@
 import dynamic from "next/dynamic";
 import React, { useState, useRef, useEffect } from "react";
-import isWebview from 'is-ua-webview';
+import isWebview from "is-ua-webview";
 
 const ReactQuill = dynamic(() => import("react-quill"), {
-  ssr: false
+  ssr: false,
 });
 
 export default function Home() {
   const [value, setValue] = useState("");
+  const [webview, setWebview] = useState('');
   const quillRef = useRef();
 
   useEffect(() => {
     const webviewChecking = isWebview(navigator.userAgent);
-    console.log(webviewChecking)
-  },[])
-
+    setWebview(webviewChecking);
+  }, []);
 
   useEffect(() => {
     console.log(quillRef.current);
   }, [quillRef]);
 
   return (
-    <ReactQuill ref={quillRef} theme="snow" value={value} onChange={setValue} />
+    <>
+      <ReactQuill
+        ref={quillRef}
+        theme="snow"
+        value={value}
+        onChange={setValue}
+      />
+      <p>{webview.toString()}</p>
+    </>
   );
 }
